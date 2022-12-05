1f559e969d09bd0f7e5b229e7614f45b86166e0b
Alexander Shilenkov <alexesc154@gmail.com>
2022-12-06 00:46:46 +0300

    add latlong2 pub

--- birds_map/lib/screens/map_screen.dart
+++ birds_map/lib/screens/map_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapScreen extends StatelessWidget {
   @override
@@ -8,7 +9,17 @@ class MapScreen extends StatelessWidget {
       body: FlutterMap(
         options: MapOptions(
           center: LatLng(0, 0),
+          zoom: 15,
+          maxZoom: 17,
+          minZoom: 3,
         ),
+        children: [
+          TileLayer(
+            urlTemplate: "https://tile.openstreetmap.org/{z}/{x}/{y}.png",
+            userAgentPackageName: 'dev.fleaflet.flutter_map.example',
+            retinaMode: true,
+          ),
+        ],
       ),
     );
   }
