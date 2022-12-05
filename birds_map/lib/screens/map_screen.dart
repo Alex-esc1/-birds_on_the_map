58ee588924ea21b6f7e3d35211013a1f31a7885c
Alexander Shilenkov <alexesc154@gmail.com>
2022-12-06 00:38:01 +0300

    add flutter_map pub

--- birds_map/lib/screens/map_screen.dart
+++ birds_map/lib/screens/map_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart';
 
 class MapScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: FlutterMap(
+        options: MapOptions(
+          center: LatLng(0, 0),
+        ),
+      ),
+    );
   }
 }
